823a23c59d5972a812bcfb399289a57ad5f79eb0
ngoiefranssen <ngoiefranssen04@gmail.com>
2024-06-07 11:30:56 +0200

    some change

--- gulpfile.js
+++ gulpfile.js
@@ -14,10 +14,7 @@ const comments = require("gulp-header-comment");
 const template = require("gulp-template");
 const theme = require("./src/theme.json");
 const node_env = process.argv.slice(2)[0];
-const headerComments = `WEBSITE: https://themefisher.com
-                        TWITTER: https://twitter.com/themefisher
-                        FACEBOOK: https://facebook.com/themefisher
-                        GITHUB: https://github.com/themefisher/`;
+const headerComments = `GITHUB: https://github.com/ngoiefranssen/`;
 
 var path = {
   // source paths
